31768b6eef65a9dcea36f3a16cfefc07e0caa511
ShathaAlghrair <alghrairshatha4@gmail.com>
2026-07-03 13:36:16 +0300

    Add shared utilities for reading and writing JSONL

---
+++ src/utils/__init__.py
@@ -0,0 +1,10 @@
+"""Shared utilities.
+
+Public API::
+
+    from arnlp.utils import read_jsonl, write_jsonl
+"""
+
+from arnlp.utils.io import read_jsonl, write_jsonl
+
+__all__ = ["read_jsonl", "write_jsonl"]
